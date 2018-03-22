b4aa826050e7d2644ae829adddeeb33fdff6adeb
sirlyluthfi <32449911+sirlyluhfi@users.noreply.github.com>
2018-03-22 20:14:00 +0700

    membuat atribut dan behavior

--- src/praktek01/Ikan.java
+++ src/praktek01/Ikan.java
@@ -10,5 +10,20 @@ package praktek01;
  * @author Lenovo
  */
 public class Ikan {
+    String bentuk;
+    String warna;
+    String habitat;
+    Integer banyak_sisik;
+    Integer jumlah_sirip;
+    
+    
+   void CaraBerkembangBiak(){
+       System.out.println("Bentuk Ikan : "+bentuk);
+       System.out.println("warna Ikan : "+warna);
+       System.out.println("habitat Ikan : "+habitat);
+       System.out.println("banyak sisik Ikan : "+banyak_sisik);
+       System.out.println("jumlah sirip ikan : "+jumlah_sirip);
+       
+   }
     
 }
